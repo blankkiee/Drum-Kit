9583147b42334211ebed37124e99a52ced9219d6
MarkDaDave <mdmartin983@gmail.com>
2024-06-25 10:45:10 +0800

    updated

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
-// var numberofDrumbtn = document.querySelectorAll(".drum").length;
+var numberofDrumbtn = document.querySelectorAll(".drum").length;
 
-// var i = 0;
-
-// while (i < numberofDrumbtn) {
-//   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-//     alert("I got clicked");
-//   });
-
-//   i++;
-// }
-
-// document.querySelector(".w").addEventListener("click", function () {
-//   alert("I got clicked");
-// });
-
-var drumNumber = document.querySelectorAll(".drum").length;
-
-// for (i = 0; i < drumNumber; i++) {
-//   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-//     alert("I got clicked");
-//   });
-// }
 var i = 0;
 
-while (i < drumNumber) {
+while (i < numberofDrumbtn) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     alert("I got clicked");
   });
